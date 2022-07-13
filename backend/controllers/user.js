72db17aa2f534c31f73e3dfc690ff994317912f9
kimt0t0 <kimagine@laposte.net>
2022-07-13 14:25:27 +0200

    Commit 03 controller sign up et login utilisateur ok reste toujours le probleme de connexion a mongodb

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,29 +1,59 @@
 /* VARIABLES */
-const User = require('../models/user');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 /* FONCTIONS */
 /* Inscription */
 exports.signup = (req, res, next) => {
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    });
-    user.save().then(
-        () => {
-            res.status(201).json({
-                message: 'User created successfully!'
+    bcrypt.hash(req.body.password, 10) // async func de cryptage mot de passe (avec hachage x10)
+        .then(hash => { // création utilisateur
+            const user = new User({
+                email: req.body.email,
+                password: hash
             });
-        }
-    ).catch(
-        (error) => {
-            res.tatus(400).json({
-                error: error
-            });
-        }
-    );
+            user.save() // enregistrement utilisateur sur bdd
+                .then(() => {
+                    res.status(201).json({
+                        message: 'Utilisateur créé !'
+                    })
+                })
+                .catch(error => {
+                    res.status(400).json({error})
+                });
+        })
+        .catch(error => {
+            res.status(500).json({error})
+        });
 };
 
 /* Connexion */
 exports.login = (req, res, next) => {
-    // instructions
-}
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Paire login/mot de passe incorrecte'
+                });
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({
+                            message: 'Paire login/mot de passe incorrecte'
+                        });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign( // chiffrage nouveau token qui contient:
+                            {userId: user._id}, // id utilisateur
+                            'RANDOM_TOKEN_SECRET', // chaîne secrète de dev temporaire pour crypter le token
+                            {expiresIn: '24h'} // durée de validité
+                        )
+                    });
+                })
+                .catch(error => {
+                    res.status(500).json({error})
+                });
+        })
+        .catch(error => res.status(500).json({error}));
+};
